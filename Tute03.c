a5fe47ae7abd5a74b5f51e450f5b4e6075bedd6b
IT21021916 Rashmi <87628868+it21021916@users.noreply.github.com>
2021-07-19 19:50:42 +0000

    I have completed exercise_03

--- Tute03.c
+++ Tute03.c
@@ -12,8 +12,25 @@ n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
-int main() {
+int main() {//function main begins programe execution
+
+  int counter = 1 ; 
+  int sum = 0 ;
+  int n;
+   
+  printf("Enter the number here :");//prompt
+  scanf("%d",&n);//read an integer value
+   
+  while(counter<=n)//loop n times
+  {
+   	
+  sum = sum + counter;// add number to sum
+  counter++;//increment counter
+   	
+	}
+	
+	printf("Sum of numbers from 1 to %d : %d",n,sum );//print sum of numbers
   
-  return 0;
+  return 0;//end of the main function
 }
 
