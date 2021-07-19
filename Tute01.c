df8e86713f88a72c34169ee15a12ebdcfff39d2c
IT21021916 Rashmi <87628868+it21021916@users.noreply.github.com>
2021-07-19 19:35:25 +0000

    I have completed exercise_01

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,24 @@
 
 #include <stdio.h>
 
-int main() {
+int main() //function main begining
+{
+
+  int sub1 , sub2 ; //subject 1 and subject 2 marks to be entererd by the user
+	float tot , avg ; //variable in whitch total and average will be stored
+	
+	printf("Enter Subject 1 mark here :"); //prompt
+	scanf("%d",&sub1); //read an integer
+	
+	printf("Enter Subject 2 mark here :"); //prompt
+	scanf("%d",&sub2); //read an integer
+	
+	tot = sub1 + sub2 ; //assign total to tot
+	avg = tot / 2.0 ; //assign average to avg
+	
+	printf("The average of the two marks : %.2f",avg); //print average
+	
   
-  return 0;
+  return 0;//end of the main function
 }
 
