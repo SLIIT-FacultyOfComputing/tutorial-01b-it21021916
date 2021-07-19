d0ad92b6c56d847846e6b637d71ed8bfcfa97a72
IT21021916 Rashmi <87628868+it21021916@users.noreply.github.com>
2021-07-19 19:44:53 +0000

    I have completed exercise_02

--- Tute02.c
+++ Tute02.c
@@ -18,7 +18,26 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
 
-int main() {
+int main() {//function main begins programe execution
+
+  float dis , amount ;//variable declaration
+	
+	printf("Enter the distance the van has travelled : ");//prompt
+	scanf("%f",&dis);//read a float value
+	
+	if(dis<=30)
+	{
+		
+		amount = dis * 50.0 ;//variable in which amount will be stored
+	}
+	else 
+	{
+		amount = 30 * 50.0 + (dis-30) * 40.0 ;//variable in which amount will be stored
+		
+	}//end if
+	
+	printf("The amount to be paid for a rental vehicle :%.2f",amount);//print the amount
+	
   
-  return 0;
+  return 0;//end of the main function
 }
