4669d4cb59cc10c03b339e20ab2e9aae8a65ef89
IT21021916 Rashmi <87628868+it21021916@users.noreply.github.com>
2021-07-19 20:05:47 +0000

    I have completed exercise_04

--- Tute04.c
+++ Tute04.c
@@ -5,15 +5,50 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+int minimum(int no1,int no2);//function declaration
+int maximum(int no1,int no2);//function declaration
+int multiply(int no1,int no2);//function declaration
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
-   return 0;
+int main (void)//main function
+{
+	int no1, no2;//no1 and no2 to be entered by the user
+	
+	printf("Enter a value for no 1 : ");//prompt
+	scanf("%d", &no1);//read an integer value
+	
+	printf("Enter a value for no 2 : ");//prompt
+	scanf("%d", &no2);//read an integer value
+	
+	printf("%d ", minimum(no1, no2));//function calling
+	printf("%d ", maximum(no1, no2));//function calling
+	printf("%d ", multiply(no1, no2));//function calling
+	
+	return 0;//end of the main function
+
+}
+
+int minimum(int no1,int no2)//function implementation
+{
+	
+	if(no1>no2)
+	{
+		
+		return no2 ;
+	}
+	
+}
+
+int maximum(int no1,int no2)//function implementation
+{
+	
+	if(no1<no2)
+	{
+		
+		return no2 ;
+	}
+}
+
+int multiply(int no1,int no2)//function implementation
+{
+	  return no1 * no2 ;
 }
